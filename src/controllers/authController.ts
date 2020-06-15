d6089fca69308211bba5d71dfdd0102cebeccaf0
Beatriz Ferreira dos Santos <BFSantos@raiadrogasil.com>
2020-06-15 09:28:19 -0300

    adicionando token

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,13 +1,15 @@
 import User from '../models/user';
 import { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
 
 class authController{
 
     public async criarUser(req:Request, res:Response){
         try {
             const user = await User.create(req.body);
+            const token = jwt.sing({user: user.id})
             user.senha = undefined;
-            return res.json(user);
+            return res.json({user, token});
         } catch (error) {
             return res.json({mensagem: 'não foi possivel criar um novo usuario'});
         }
